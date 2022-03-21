cc1198d57512369f971974b7882f2977f07bb63c
i-walker <46971368+i-walker@users.noreply.github.com>
2022-03-21 10:16:01 +0100

    set up arrow proofs

---
+++ proofs/prelude/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+  id(libs.plugins.kotlin.multiplatform.get().pluginId)
+  alias(libs.plugins.arrowGradleConfig.kotlin)
+  alias(libs.plugins.arrowGradleConfig.publish)
+  alias(libs.plugins.arrowGradleConfig.versioning)
+}
+
+kotlin {
+  explicitApi = null
+
+  sourceSets {
+    commonMain {
+      dependencies {
+        implementation(libs.kotlin.stdlibCommon)
+      }
+    }
+
+    named("jvmMain") {
+      dependencies {
+        implementation(libs.kotlin.stdlibJDK8)
+      }
+    }
+
+    named("jsMain") {
+      dependencies {
+        implementation(libs.kotlin.stdlibJS)
+      }
+    }
+  }
+}
---
+++ proofs/proofs-plugin/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+  id(libs.plugins.kotlin.jvm.get().pluginId)
+  alias(libs.plugins.arrowGradleConfig.kotlin)
+  alias(libs.plugins.arrowGradleConfig.publish)
+  alias(libs.plugins.arrowGradleConfig.versioning)
+}
+
+kotlin {
+  explicitApi = null
+}
+
+dependencies {
+    compileOnly(libs.kotlin.stdlibJDK8)
+    implementation(libs.arrowMeta)
+
+    testImplementation(libs.kotlin.stdlibJDK8)
+    testImplementation(libs.junit)
+    testImplementation(libs.arrowMetaTest)
+    testRuntimeOnly(libs.arrowMeta)
+    //testRuntimeOnly(projects.arrowMetaPrelude)
+    testRuntimeOnly(projects.arrowProofsPlugin)
+    testRuntimeOnly(libs.arrowCore)
+}
