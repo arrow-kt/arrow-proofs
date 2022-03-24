0b3c92c4f780a0799afd9ce7a92d6dc9bf9f1f55
raulraja <raulraja@gmail.com>
2022-03-24 20:48:11 +0100

    spotless

--- proofs/proofs-plugin/src/main/kotlin/arrow/meta/ProofsMetaPlugin.kt
+++ proofs/proofs-plugin/src/main/kotlin/arrow/meta/ProofsMetaPlugin.kt
@@ -3,9 +3,7 @@ package arrow.meta
 import arrow.meta.phases.CompilerContext
 import arrow.meta.plugins.proofs.typeProofs
 
-/**
- * Arrow Proofs plugin.
- */
+/** Arrow Proofs plugin. */
 open class ProofsMetaPlugin : Meta {
   override fun intercept(ctx: CompilerContext): List<CliPlugin> = listOf(typeProofs)
 }
