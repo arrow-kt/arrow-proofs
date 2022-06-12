d25375f3e25545f975e84cad983b705dd4d641bf
Bloder <aptgetinstalldanielleme@gmail.com>
2022-06-12 08:01:00 -0300

    WIP: Support for context function with recursive type parameters (#18)
    * Finish recursive support to IR generation
    
    * Add temporary solution into context receivers test to make it pass
    
    * Create test for two type param context function
    
    * Create tests for context function with more than two type parameters

--- arrow-inject-compiler-plugin/src/testData/box/context-receivers/context_receivers_with_more_than_two_type_parameters.kt
+++ arrow-inject-compiler-plugin/src/testData/box/context-receivers/context_receivers_with_more_than_two_type_parameters.kt
@@ -4,27 +4,28 @@ import arrow.inject.annotations.context
 import arrow.inject.annotations.Provider
 import arrow.inject.annotations.contextual
 
-@Provider
-class Persistence
+@Provider class Persistence
+@Provider class X
+@Provider class Y
+@Provider class Z
+@Provider class W
 
-context(Persistence)
+context(Persistence, X, Y, Z, W)
 class Repo(val x: Int)
 
-fun f2(): Int {
-  println("will drop from nested body")
-  context<Persistence>()
+fun f(): Int {
+  println("123")
+  context<Persistence, X, Y, Z, W>()
   return Repo(0).x
 }
 
-fun workaround(): Int {
+fun f2(): Int {
   println("will drop from nested body")
-  return contextual<Persistence, Int>(Persistence()) {
-    Repo(0).x
-  }
+  return contextual<Persistence, Int>(Persistence()) { 2 }
 }
 
 fun box(): String {
-  val result = f2()
+  val result = f()
   return if (result == 0) {
     "OK"
   } else {
--- arrow-inject-compiler-plugin/src/testData/box/context-receivers/context_receivers_with_two_type_parameters.kt
+++ arrow-inject-compiler-plugin/src/testData/box/context-receivers/context_receivers_with_two_type_parameters.kt
@@ -4,27 +4,25 @@ import arrow.inject.annotations.context
 import arrow.inject.annotations.Provider
 import arrow.inject.annotations.contextual
 
-@Provider
-class Persistence
+@Provider class Persistence
+@Provider class X
 
-context(Persistence)
+context(Persistence, X)
 class Repo(val x: Int)
 
-fun f2(): Int {
-  println("will drop from nested body")
-  context<Persistence>()
+fun f(): Int {
+  println("123")
+  context<Persistence, X>()
   return Repo(0).x
 }
 
-fun workaround(): Int {
+fun f2(): Int {
   println("will drop from nested body")
-  return contextual<Persistence, Int>(Persistence()) {
-    Repo(0).x
-  }
+  return contextual<Persistence, Int>(Persistence()) { 2 }
 }
 
 fun box(): String {
-  val result = f2()
+  val result = f()
   return if (result == 0) {
     "OK"
   } else {
